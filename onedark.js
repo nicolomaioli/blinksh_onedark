fe6af55131f70b9defea5a9c0c540d559b5dfb12
Nicolo Maioli <nicolomaioli@gmail.com>
2020-05-12 23:47:50 +0000

    First commit

---
+++ onedark.js
@@ -0,0 +1,22 @@
+t.prefs_.set('color-palette-overrides', [
+	"#4B5263",
+	"#BE5046",
+	"#98C379",
+	"#D19A66",
+	"#61AFEF",
+	"#C678DD",
+	"#56B6C2",
+	"#3E4452",
+	"#5C6370",
+	"#E06C75",
+	"#98C379",
+	"#E5C07B",
+	"#61AFEF",
+	"#C678DD",
+	"#56B6C2",
+	"#ABB2BF"
+]);
+
+t.prefs_.set('cursor-color', 'rgb(171, 178, 191, 0.5)');
+t.prefs_.set('foreground-color', '#ABB2BF');
+t.prefs_.set('background-color', '#282C34');
